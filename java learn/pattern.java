121193c5a9ab074659e546ba7dac0589780e5ec5
Piyush Prakash <piyushprakash2104@gmail.com>
2026-01-27 00:51:28 +0530

    pattern printed

--- java learn/pattern.java
+++ java learn/pattern.java
@@ -2,9 +2,11 @@ import java.util.*;
 
 class pattern {
     public static void main(String args[]) {
-        for (int i = 1; i <= 15; i++) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        for (int i = 1; i <= n; i++) {
             for (int j = 1; j <= i; j++) {
-                System.out.print("z" + " ");
+                System.out.print("*" + " ");
             }
             System.out.println();
         }
